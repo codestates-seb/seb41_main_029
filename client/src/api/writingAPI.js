24aa3bd247f1197c507bdb135a020ee23140d6cd
psc <whirled7@gmail.com>
2023-01-24 17:57:11 +0900

    fix : 주석처리

--- client/src/api/writingAPI.js
+++ client/src/api/writingAPI.js
@@ -2,19 +2,19 @@ import axios from "axios";
 
 const url = `http://ec2-13-209-237-254.ap-northeast-2.compute.amazonaws.com:8080/`;
 
-// export const postWriting = async (Token) => {
-//   try {
-//     const res = await axios({
-//       method: "post",
-//       url: `${url}boards/articles`,
-//       // Token이 있어야 접속 가능
-//       headers: { Authorization: `Bearer ${Token}` },
-//     });
-//     return res.data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+export const postWriting = async (Token) => {
+  try {
+    const res = await axios({
+      method: "post",
+      url: `${url}boards/articles`,
+      // Token이 있어야 접속 가능
+      headers: { Authorization: `Bearer ${Token}` },
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const getWriting = async (Token, boardSeq) => {
   try {
