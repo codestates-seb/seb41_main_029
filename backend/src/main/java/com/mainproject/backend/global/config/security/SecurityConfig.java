5dd387a313b84a5a3e3d22a55e7cadb838caa044
MWJOB <okkilltogo@gmail.com>
2023-01-30 16:59:27 +0900

    feat. get / 추가 제거

--- backend/src/main/java/com/mainproject/backend/global/config/security/SecurityConfig.java
+++ backend/src/main/java/com/mainproject/backend/global/config/security/SecurityConfig.java
@@ -68,6 +68,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/").permitAll()
                 .antMatchers(HttpMethod.GET, "/image/**").permitAll()
                 .antMatchers("/users/signup", "/users/**").permitAll()
                 .antMatchers("/uploadFiles").permitAll()
