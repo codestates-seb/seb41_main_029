6b59561dc642b3d5a549c0bcb6b1c75f938901f8
sywoo0109 <sywoo0109@gmail.com>
2023-01-31 02:24:27 +0900

    feat: 갤러리 페이지 레이아웃 구상 및 목업 제작

---
+++ client/src/page/Test.js
@@ -0,0 +1,10 @@
+import SwiperComponent from "../component/Swiper";
+
+export default function Test() {
+  return (
+    <>
+      <div>테스트용 페이지</div>
+      <SwiperComponent></SwiperComponent>
+    </>
+  );
+}
